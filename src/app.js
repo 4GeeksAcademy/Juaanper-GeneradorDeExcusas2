ad1fb246c4a375da2e1024b1488ab8003c898430
Juan Perez Rodriguez <juaanperez17@gmail.com>
2025-05-11 18:27:42 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+      function generarExcusa (){
+      let sujeto= ["Mi Perro", "Mi Abuelita" , "El Mensajero" , "El Aguila" ];
+      let accion= ["Comio", "Orino", "Aplasto", "Rompio"];
+      let objeto= ["Mi tarea" , "Mi Movil" , "Mi Carro", "El Mueble"];
+      let momento=["Despues de Clases" , "Cuando estaba durmiendo" , "Mientras hacia ejercicio" , "Durante mi almuerzo" , "Mientras estaba orando"];
+
+      let personas = sujeto [Math.floor(Math.random()*sujeto.length)];
+      let reaccion = accion [Math.floor(Math.random()*accion.length)];
+      let instrumento = objeto [Math.floor(Math.random()*objeto.length)];
+      let cuando = momento [Math.floor(Math.random()*momento.length)];
+
+      let excusa = `${personas} ${reaccion} ${instrumento} ${cuando}`;
+
+      document.querySelector("#excuse").innerHTML = excusa
+    }
+    generarExcusa()
   console.log("Hello Rigo from the console!");
-};
+}
